18afc0e2c5d183e085040584c3762ea4ecc96446
Rafael Luiz Parenza <rafaelparenza@hotmail.com>
2024-02-06 20:25:31 -0300

    Novidade sumula

---
+++ BlazorApp3/Classes.cs
@@ -0,0 +1,50 @@
+﻿using static BlazorApp3.Pages.Index;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp3
+{
+    public class objSumula
+    {
+        public objSumula(int i)
+        {
+            jogadoresTimeA = new();
+            jogadoresTimeA.Add(new("GK"));
+            jogadoresTimeA.Add(new("ZAG"));
+            jogadoresTimeA.Add(new("MID"));
+            jogadoresTimeA.Add(new("ATK"));
+
+            jogadoresTimeB = new();
+            jogadoresTimeB.Add(new("GK"));
+            jogadoresTimeB.Add(new("ZAG"));
+            jogadoresTimeB.Add(new("MID"));
+            jogadoresTimeB.Add(new("ATK"));
+        }
+        public objSumula()
+        {
+        }
+        public string nomeTimeA { get; set; } = "";
+        public string nomeTimeB { get; set; } = "";
+        [JsonIgnore]
+        public int placarTimeA { get { return jogadoresTimeB.Select(a => a.golsContra).Sum() + jogadoresTimeA.Select(a => a.gols).Sum(); } }
+        [JsonIgnore]
+        public int placarTimeB { get { return jogadoresTimeA.Select(a => a.golsContra).Sum() + jogadoresTimeB.Select(a => a.gols).Sum(); } }
+        public List<Jogadores> jogadoresTimeA { get; set; } = new();
+        public List<Jogadores> jogadoresTimeB { get; set; } = new();
+        [JsonIgnore]
+        public List<Jogadores> todosJogadores
+        {
+            get
+            {
+                var jogadores = new List<Jogadores>();
+                jogadores.AddRange(jogadoresTimeA);
+                jogadores.AddRange(jogadoresTimeB);
+                return jogadores;
+            }
+        }
+
+
+        [JsonIgnore]
+        public string sssumula { get; set; }
+        public string Anotacoes { get; set; } = "";
+    }
+}
---
+++ BlazorApp3/wwwroot/script.js
@@ -0,0 +1,62 @@
+﻿
+
+function base64ToBlob(base64) {
+    const byteCharacters = atob(base64);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512);
+
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, { type: 'application/octet-stream' });
+}
+window.BlazorDownloadFile = {
+    saveAsFile: function (filename, dataBase64) {
+        const link = document.createElement('a');
+        link.download = filename;
+
+        // Cria um blob a partir dos dados base64
+        const blob = base64ToBlob(dataBase64);
+        const url = window.URL.createObjectURL(blob);
+
+        link.href = url;
+        document.body.appendChild(link);
+        link.click();
+
+        // Limpa o objeto URL
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+    }
+};
+window.exibirPrompt = function (mensagem) {
+    return prompt(mensagem);
+};
+window.exibirAlerta = function (mensagem) {
+    alert(mensagem);
+};
+
+window.cookieFunctions = {
+    getCookie: function (key) {
+        return document.cookie.replace(new RegExp("(?:(?:^|.*;\\s*)" + key + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1");
+    },
+    setCookie: function (key, value, expireDays) {
+        var expires = "";
+        if (expireDays) {
+            var date = new Date();
+            date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = key + "=" + (value || "") + expires + "; path=/";
+    },
+    removeCookie: function (key) {
+        document.cookie = key + '=; Max-Age=-99999999;';
+    }
+};
