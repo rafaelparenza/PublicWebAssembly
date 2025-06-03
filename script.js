c23bc6ba90903ffa9045aa9d9521daf5a0abc60f
Rafael Parenza <48654547+rafaelparenza@users.noreply.github.com>
2025-06-02 21:28:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function base64ToBlob(base64) {
     const byteCharacters = atob(base64);
     const byteArrays = [];
+    <!--teste-->
 
     for (let offset = 0; offset < byteCharacters.length; offset += 512) {
         const slice = byteCharacters.slice(offset, offset + 512);
